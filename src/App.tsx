5498e855edcac22d4954fe5b710159b376e72b7c
VukGr <vuk.grujic.5@hotmail.com>
2024-08-22 18:58:58 +0200

    Add eslint and prettier.
    Run prettier on all files

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 import './App.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Button } from '@/components/ui/button'
+
+function Test({ text }: { text: string }) {
+  return <Button>{text}</Button>
+}
 
 function Main() {
   const [count, setCount] = useState(0)
@@ -9,6 +13,7 @@ function Main() {
   return (
     <>
       <h1>Vite + React</h1>
+      <Test text='Test' />
       <div className='card'>
         <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -20,7 +25,7 @@ function Main() {
 
 function App() {
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <ThemeProvider defaultTheme='dark' storageKey='vite-ui-theme'>
       <Main />
     </ThemeProvider>
   )
