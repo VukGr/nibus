34b1eaecc3b165308a74ff1e9d54e6a9c737a3c7
VukGr <vuk.grujic.5@hotmail.com>
2024-08-22 18:58:58 +0200

    Minor change

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,25 @@ import './App.css'
 import { ThemeProvider } from '@/components/theme-provider'
 import { Button } from '@/components/ui/button'
 
-function App() {
+function Main() {
   const [count, setCount] = useState(0)
 
   return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <>
       <h1>Vite + React</h1>
-      <div className="card">
+      <div className='card'>
         <Button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </Button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    </>
+  )
+}
+
+function App() {
+  return (
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Main />
     </ThemeProvider>
   )
 }
