5f3e6fd72c14f34d55f8aed7915c32e5bd00efa0
VukGr <vuk.grujic.5@hotmail.com>
2024-08-22 18:58:58 +0200

    Add github actions workflow for deployment.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import eslint from 'vite-plugin-eslint2'
 
 export default defineConfig({
   plugins: [react(), eslint()],
+  base: '/nibus/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
