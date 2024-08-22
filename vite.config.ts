3d0b5d932acbbb999754913dc104db5a9811c090
VukGr <vuk.grujic.5@hotmail.com>
2024-08-22 18:58:58 +0200

    Fix build issue with vite-plugin-eslint

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
-import eslint from 'vite-plugin-eslint'
+import eslint from 'vite-plugin-eslint2'
 
 export default defineConfig({
   plugins: [react(), eslint()],
